cb4955f6d1baf5b7ce4dacbbd027a01f2473bd61
daratequila <daratequila@gmail.com>
2021-06-21 13:48:39 +0200

    init

--- lib/wp-api.dart
+++ lib/wp-api.dart
@@ -1,7 +1,7 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-var url = 'https://eosmrtnice.ba/wp-json/wp/v2/osmrtnica';
+var url = 'https://eosmrtnice.ba/wp-json/wp/v2/osmrtnica?per_page=50';
 Future<List> fetchWpPosts() async {
   final response =
       await http.get(Uri.parse(url), headers: {"Accept": "application/json"});
